5803a65b1d784565f00e9a45a5da080a8e95f6ed
mahi13singh2004 <mahi13singh2004@gmail.com>
2026-03-04 01:33:31 +0530

    git added frontend url

--- frontend/src/utils/axiosService.js
+++ frontend/src/utils/axiosService.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
-    timeout: 30000,
+    baseURL: "https://moviekhoj-backend.onrender.com",
     withCredentials: true
 });
 
